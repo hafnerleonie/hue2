4658050d03ca0708ff243438125592d4d3dca530
hafnerleonie <hafner.leonie.2006@gmx.at>
2022-09-23 10:39:34 +0200

    2. commit: improved oldForEach()

--- src/net/htlgkr/hafle/pos3/hue02/HalloJavamitForEach.java
+++ src/net/htlgkr/hafle/pos3/hue02/HalloJavamitForEach.java
@@ -1,16 +1,20 @@
 package net.htlgkr.hafle.pos3.hue02;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HalloJavamitForEach {
-    private List<String> list;
+    private List<String> list = new ArrayList<>();
     public static void main(String[] args) {
         HalloJavamitForEach halloJavamitForEach = new HalloJavamitForEach();
         halloJavamitForEach.oldForEach();
     }
     private void oldForEach(){
+        list.add("first");
+        list.add("second");
+        list.add("third");
         for (String n:list) {
-            System.out.println(list);
+            System.out.println(n);
         }
     }
 }
