9b72fe8a98b6d4dc50eb757ea71a4abfb1538b8e
Leonie Hafner <hafner.leonie.2006@gmx.at>
2022-09-29 20:38:30 +0200

    10. commit: VectorCalculator implemented

---
+++ src/net/htlgkr/hafle/pos3/hue02/VectorCalculator.java
@@ -0,0 +1,29 @@
+package net.htlgkr.hafle.pos3.hue02;
+
+public class VectorCalculator extends AbstractCalculator{
+
+
+    public VectorCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
+}
