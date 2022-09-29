e04231c7451b66d42d0f7b8ded048da431c9b058
Leonie Hafner <hafner.leonie.2006@gmx.at>
2022-09-29 18:53:47 +0200

    5. commit: NumberTester improved

--- src/net/htlgkr/hafle/pos3/hue02/NumberTester.java
+++ src/net/htlgkr/hafle/pos3/hue02/NumberTester.java
@@ -1,12 +1,21 @@
 package net.htlgkr.hafle.pos3.hue02;
 
 public class NumberTester {
-    NumberTest oddTester;
-    NumberTest primeTester;
-    NumberTest palindromeTester;
+    NumberTest oddTester = number -> number % 2 == 0;
+    NumberTest primeTester = number -> number == 2 || number % 2 == 1;
+    NumberTest palindromeTester = number -> {
+        int temp = 0;
+        while (number != 0){
+            int temp2 = number % 10;
+            temp = temp * 10 + temp2;
+            number = number / 10;
+        }
+        return number == temp;
+    };
+    String fileName;
 
     public NumberTester ( String fileName ) {
-        // TODO
+        this.fileName = fileName;
 
     }
     public void setOddEvenTester (NumberTest oddTester ) {
